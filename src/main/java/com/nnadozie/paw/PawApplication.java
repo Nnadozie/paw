0575d9fc91c999e3966a60afd47172b39e7d3320
Nnadozie <n.e.okeke@outlook.com>
2020-04-26 20:17:39 +0100

    blank setup

---
+++ src/main/java/com/nnadozie/paw/PawApplication.java
@@ -0,0 +1,14 @@
+package com.nnadozie.paw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawApplication {
+
+	public static void main(String[] args) {
+		System.out.println("I started");
+		//SpringApplication.run(PawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnadozie/paw/PawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnadozie.paw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
