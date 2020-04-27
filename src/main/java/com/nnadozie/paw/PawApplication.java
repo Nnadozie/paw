8e64148cad4e76e7461d3e5205b96e0a7c8b7dca
Nnadozie <n.e.okeke@outlook.com>
2020-04-27 13:23:35 +0100

    added jpa and security dependencies

--- src/main/java/com/nnadozie/paw/PawApplication.java
+++ src/main/java/com/nnadozie/paw/PawApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PawApplication {
 
 	public static void main(String[] args) {
-		System.out.println("I started");
-		//SpringApplication.run(PawApplication.class, args);
+		SpringApplication.run(PawApplication.class, args);
 	}
 
 }
