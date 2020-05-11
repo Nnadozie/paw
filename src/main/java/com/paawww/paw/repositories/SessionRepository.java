09f6388cdd2b2bf1f264d58dd8b877f898275bef
Nnadozie <n.e.okeke@outlook.com>
2020-05-11 20:00:54 +0100

    removed tutorial code

--- src/main/java/com/paawww/paw/repositories/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paawww.paw.repositories;
-
-import com.paawww.paw.models.Session;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-    
-}
