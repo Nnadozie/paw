bfab30e8c8706b244a0d3d7321ec355bc47e05fb
Nnadozie <n.e.okeke@outlook.com>
2020-06-12 11:24:30 +0100

    added assertj to use in testing

--- src/test/java/com/paawww/paw/controllers/UserDataRepositoryIntegrationTest.java
+++ src/test/java/com/paawww/paw/controllers/UserDataRepositoryIntegrationTest.java
@@ -1,26 +1,37 @@
 package com.paawww.paw.controllers;
 
-import org.junit.jupiter.api.Test;
+import com.paawww.paw.models.UserDataModel;
+import com.paawww.paw.repositories.UserDataRepository;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(CreateUserController.class)
-public class CreateUserTests {
+@DataJpaTest
+public class UserDataRepositoryIntegrationTest {
     @Autowired
-    //private MockMvc mockMvc;
+    private TestEntityManager entityManager;
 
-    //@MockBean
-    //UserService userService;
+    @Autowired
+    private UserDataRepository userDataRepository;
 
     @Test
-    public void createNewUser() throws Exception {
-        // mockMvc.perform(post("/auth/create-user"))
-        //         .andExpect(Status().isOk())
-        //         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        //         .andExpect(content().json("[]"));
+    public void whenGetUserByName_thenReturnUser() {
+        UserDataModel nnadozie = new UserDataModel();
+        nnadozie.setFirstName("nnadozie");
+        nnadozie.setLastName("Okeke");
+
+        entityManager.persist(nnadozie);
+        entityManager.flush();
+
+        UserDataModel found = userDataRepository.getUserByName(nnadozie.getFirstName(), nnadozie.getLastName());
+        
+        assertThat(found.getFirstName()).isEqualTo(nnadozie.getFirstName());
     }
     
 }
