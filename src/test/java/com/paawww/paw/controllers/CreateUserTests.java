6db9e38dc94856dc715de26271ffe8dc25e9a0b6
Nnadozie <n.e.okeke@outlook.com>
2020-06-12 09:56:50 +0100

    refactoring in progress

--- src/test/java/com/paawww/paw/controllers/CreateUserTests.java
+++ src/test/java/com/paawww/paw/controllers/CreateUserTests.java
@@ -4,18 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import ch.qos.logback.core.status.Status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CreateUserController.class)
 public class CreateUserTests {
     @Autowired
-    private MockMvc mockMvc;
+    //private MockMvc mockMvc;
 
     //@MockBean
     //UserService userService;
